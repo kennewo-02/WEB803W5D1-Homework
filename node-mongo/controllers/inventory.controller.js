e6a3b47b59bfbfb7260e26a62620248789761924
kennewo-02 <ccchzoom@gmail.com>
2026-02-03 01:04:28 -0600

    Commit Wk5d1 homework

---
+++ node-mongo/controllers/inventory.controller.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+const Inventory = require('../models/inventory.model.js');
+
+exports.createInventory = (req,res) => {
+    const inventory = new Inventory({
+        prodname: req.body.prodname,
+        qty: req.body.qty,
+        price: req.body.price,
+        status: req.body.status,
+    });
+
+    //Save a Inventory in the MongoDB
+    inventory.save().then(data => {
+        res.status(200).json(data);
+    }).catch(err => {
+        res.status(500).json({
+            message: "Fail!",
+            error: err.message
+        });
+    });
+};
+exports.getInventory = (req,res) =>{
+       Inventory.findById(req.params.id).select('-__v')
+       .then(inventory => {
+        res.status(200).json(inventory);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(400).send({
+            message: "Inventory not found with id " + req.params.id,
+            error: err
+        });
+    }
+    return res.status(500).send({
+        message: "Error retrieving Inventory with id " + req.params.id,
+        error: err
+    });
+    });
+};
+exports.inventories = (req,res) => {
+        Inventory.find().select('-__v').then(inventoryInfos => {
+        res.status(200).json(inventoryInfos);
+    }).catch(err => {
+        // log on console
+        console.log(err);
+        res.status(500).json({
+            message:"Error!",
+            error: err
+        });
+    });
+};
+exports.deleteInventory = (req, res) =>{
+    Inventory.findByIdAndRemove(req.params.id).select('-__v-_id')
+       .then(inventory => {
+        if(!inventory){
+        res.status(404).json({
+            message: "No inventory found with id = " + req.params.id,
+            error: "404",
+        });
+    }
+        res.status(200).json({});
+    }).catch(err => {
+        return res.status(500).send({
+            message: "Error -> can't delete inventory with id " + req.params.id,
+            error: err.message
+        });
+    });
+};
+
+exports.updatedInventory = (req,res) => {
+    //Find inventory and update it
+ Inventory.findByIdAndUpdate(
+    req.body.id,
+    {
+        prodname: req.body.prodname,
+        qty: req.body.qty,
+        price: req.body.price,
+        status: req.body.status,
+    },
+    {new: true}
+).select('-__v')
+       .then(inventory => {
+        if(!inventory){
+       return res.status(404).send({
+            message: "Error -> can't update inventory with id = " + req.params.id,
+            error: "Not Found!",
+        });
+    }
+        res.status(200).json(inventory);
+    }).catch(err => {
+        return res.status(500).send({
+            message: "Error -> can't update inventory with id " + req.params.id,
+            error: err.message
+        });
+    });
+};
+
